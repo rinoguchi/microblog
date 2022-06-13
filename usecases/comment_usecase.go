614ad93f511bacf2fbbcf29963b4e5a51479e3de
rinoguchi <inogu1979@gmail.com>
2022-06-13 09:42:41 +0900

    コメントを扱うユースケースを作成

---
+++ usecases/comment_usecase.go
@@ -0,0 +1,42 @@
+package usecases
+
+import (
+	"context"
+
+	"github.com/rinoguchi/microblog/entities"
+)
+
+type CommentUsecase struct {
+	commentRepository entities.CommentRepository
+}
+
+func (p *CommentUsecase) AddComment(ctx context.Context, newComment *NewComment) (*Comment, error) {
+	commentEntity, err := p.commentRepository.Add(ctx, &entities.CommentEntity{
+		Text: newComment.Text,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return toComment(&commentEntity), nil
+}
+
+func (p *CommentUsecase) FindAllComment(ctx context.Context, id int64) ([]*Comment, error) {
+	commentEntities, err := p.commentRepository.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	comments := []*Comment{}
+	for _, commentEntity := range *commentEntities {
+		comments = append(comments, toComment(&commentEntity))
+	}
+	return comments, nil
+}
+
+func toComment(commentEntity *entities.CommentEntity) *Comment {
+	return &Comment{
+		Id:        commentEntity.Id,
+		Text:      commentEntity.Text,
+		CreatedAt: commentEntity.CreatedAt,
+		UpdatedAt: commentEntity.UpdatedAt,
+	}
+}
