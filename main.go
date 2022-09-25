d327547fe91e908d27911663fc260fd0d66f58c9
rinoguchi <inogu1979@gmail.com>
2022-09-25 18:05:59 +0900

    cors対策

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	middleware "github.com/deepmap/oapi-codegen/pkg/chi-middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/rinoguchi/microblog/adapters/controllers"
 )
 
@@ -20,8 +22,18 @@ func main() {
 	swagger.Servers = nil
 	server := InitializeServer()
 	router := chi.NewRouter()
+
+	allowOrigins := strings.Split(os.Getenv("ALLOW_ORIGINS"), ",")
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: allowOrigins,
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		MaxAge:         300,
+	}))
+
 	router.Use(middleware.OapiRequestValidator(swagger)) // validationを設定
-	controllers.HandlerFromMux(server, router)           // chiのrouterと実装したserverを紐付け
+	router.Use(server.SetTxMiddleware)                   // トランザクションをcontextに設定
+
+	controllers.HandlerFromMux(server, router) // chiのrouterと実装したserverを紐付け
 	port := ":8080"
 	println("starting server port" + port)
 	http.ListenAndServe(port, router)
